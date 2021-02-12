c0e3e84cc606e5b3c561936cb6bf2272e05f7497
David Polcari <dpolcari18@gmail.com>
2021-02-12 13:57:23 -0600

    create user with validations complete

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -4,6 +4,26 @@ class PassengersController < ApplicationController
         @passenger = Passenger.find_by(id: params[:id])
     end
 
+    def new
+        @passenger = Passenger.new
 
+    end
+
+    def create
+        @passenger = Passenger.new(passenger_params)
+        if @passenger.valid?
+            @passenger.save
+            redirect_to passenger_path(@passenger)
+        else
+            flash[:error] = @passenger.errors.full_messages
+            redirect_to new_passenger_path
+        end
+    end
+
+    private
+
+    def passenger_params
+        params.require(:passenger).permit(:first_name, :last_name, :username, :password, :age)
+    end
 end
 
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,6 +2,13 @@ class Passenger < ApplicationRecord
     has_many :tickets
     has_many :flights, through: :tickets
 
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true, length: {in: 6..20}
+    validates :age, presence: true, inclusion: {in: 1..110}
+
+
     def full_name
         "#{self.first_name} #{self.last_name}"
     end
