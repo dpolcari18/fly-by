665490e33338adb44514f8af4f3a507519be3055
David Polcari <dpolcari18@gmail.com>
2021-02-12 12:22:18 -0600

    cant login without password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,13 @@ class SessionsController < ApplicationController
     def create
         # byebug
         @passenger = Passenger.find_by(username: params[:username])
-        session[:passenger_id] = @passenger.id
-        redirect_to passenger_path(@passenger)
+        if @passenger.password == params[:password]
+            session[:passenger_id] = @passenger.id
+            redirect_to passenger_path(@passenger)
+        else
+            flash[:error] = @passenger.errors.full_messages
+            redirect_to '/'
+        end
     end
 
     def destroy
