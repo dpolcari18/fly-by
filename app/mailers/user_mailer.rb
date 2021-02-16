53df0ea2de73d4daa0c8cb5d3ecac6c489786f08
HannaBM12 <Hannabmulugeta@gmail.com>
2021-02-16 12:50:44 -0500

    styling updated

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,11 @@
+class UserMailer < ApplicationMailer
+
+    default from: 'notifications@example.com'
+
+    def welcome_email
+        passenger = params[:passenger]
+        @url  = '/'
+        mail(to: @passenger.email, subject: 'Welcome to My Awesome Site')
+    end
+
+end
