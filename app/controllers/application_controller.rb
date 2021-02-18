a4ec298107a2ef15f83169020cc10ba942f895cb
David Polcari <dpolcari18@gmail.com>
2021-02-18 11:14:31 -0600

    company logos added git status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
     before_action :current_passenger, only: [:logged_in?, :require_logged_in]
     before_action :current_employee, only: [:employee_logged_in?, :require_employee_logged_in]
 
+    helper_method :company_logo
+
     def current_passenger
         @passenger = (Passenger.find_by(id: session[:passenger_id]) || Passenger.new)
     end
@@ -26,5 +28,21 @@ class ApplicationController < ActionController::Base
 
     def require_employee_logged_in
         redirect_to(controller: 'sessions', action: 'new') unless employee_logged_in?
+    end
+
+    def company_logo
+        airline = Airline.find_by(id: session[:employee_id])
+        case airline.name
+        when "United"
+            "united.jpg"
+        when "Delta"
+            "delta.jpg"
+        when "Southwest"
+            "southwest.jpg"
+        when "American"
+            "american.jpg"
+        when "Alaska"
+            "alaska.jpg"
+        end
     end
 end
